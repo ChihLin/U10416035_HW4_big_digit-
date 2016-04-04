4989e6732ced6f94a3b7b2f43624ed671a2095ec
ChihLin <azs128821@gmail.com>
2016-04-04 22:16:05 +0800

    add the different length ' s plus
    add the two arraylist to store the two string
    because it has the different size
    so use the arraylist to add "0" 
    to keep the two string have the same size
    then add them and check they are not bigger than 10
    then print it.

--- Decimal.java
+++ Decimal.java
@@ -1,3 +1,5 @@
+//U10416035 Cheng Chih Lin
+
 
 import java.lang.*;
 import java.util.*;
@@ -25,6 +27,7 @@ public class Decimal
 		//case one is the two numbers are equal
 		if(x.length() == y.length()){
 			
+			
 			//put the numbers into array
 			for(int i = x.length() -1 ; i >=0  ; i--) {
 
@@ -45,8 +48,9 @@ public class Decimal
 				//System.out.print(c);	
 				
 			}
+			
 			// if the digit more than 10 will add 1 to the left digit
-			for(int i = 0; i  < x.length()-1 ; i++) {
+			for(int i = 0; i  < list.size()-1 ; i++) {
 				if(list.get(i) >=10){	
 					//use the [java.util.ArrayList.set(index , element) method]
 					list.set( i+1 ,list.get(i+1) +1);
@@ -54,12 +58,123 @@ public class Decimal
 			
 				}
 			}
+			
 			//print the ans
 			System.out.print("the fist number plus the second number is = ");
 			for(int j = list.size()-1 ; j >=0 ; j--){
+				
 				System.out.print(list.get(j));
 			}
 			
 		}
-	}	
-}	
+		
+		//case two is the first number 's length is longer than the other
+		if(x.length() > y.length()){
+			
+			// In order to put "0" into the list (keep the two string has the same length) ,
+			//to put them into arraylist , thus ,
+			//to put "0" into the String so the type need convert to int			
+
+			ArrayList<Integer> list1 = new ArrayList<>();
+			ArrayList<Integer> list2 = new ArrayList<>();
+			
+
+			//put the first number into the list1
+			//int order to add"0" into the String so the type need convert to int
+			
+			for(int i = x.length()-1 ; i >=0 ; i-- ) {
+				String s1 = String.valueOf(x.charAt(i));
+				list1.add(Integer.valueOf(s1));
+			}
+			
+			//put the first number into the list2
+			for(int i  = y.length()-1; i >=0 ; i--){	
+				String s2 = String.valueOf(y.charAt(i));
+				list2.add(Integer.valueOf(s2));
+				
+			}
+			//because of the first number is longer than the second number 
+			//add "0" into the second number to keep the two number has the same length
+			for(int i = list2.size() ; i< list1.size() ; i++){
+				
+				list2.add(0);
+			}
+			
+			//plus the two numbers
+			for(int i = 0 ; i < list1.size() ; i++){
+				int plus = list1.get(i) + list2.get(i);
+				list.add(plus);
+				
+			}
+			
+			
+			for(int i = 0; i  < list.size()-1 ; i++) {
+				if(list.get(i) >=10){	
+					//use the [java.util.ArrayList.set(index , element) method]
+					list.set( i+1 ,list.get(i+1) +1);
+					list.set(i , list.get(i) % 10);
+			
+				}
+			}
+			
+			//print the ans
+			System.out.print("the fist number plus the second number is = ");
+			
+			//print the number 
+			for(int j = list.size()-1 ; j >=0 ; j--){
+				System.out.print(list.get(j));
+			}
+			
+		
+			
+		}
+		
+		
+		if(x.length() < y.length()){
+			
+			ArrayList<Integer> list1 = new ArrayList<>();
+			ArrayList<Integer> list2 = new ArrayList<>();
+			
+			
+			for(int i = x.length()-1 ; i >=0 ; i-- ) {
+				String s1 = String.valueOf(x.charAt(i));
+				list1.add(Integer.valueOf(s1));
+			}
+			
+			//put the first number into the list2
+			for(int i  = y.length()-1; i >=0 ; i--){	
+				String s2 = String.valueOf(y.charAt(i));
+				list2.add(Integer.valueOf(s2));
+				
+			}
+			
+			for(int i = list1.size() ; i< list2.size() ; i++){
+				list1.add(0);
+				
+			}
+			
+			for(int i = 0 ; i < list2.size() ; i++){
+				int plus = list1.get(i) + list2.get(i);
+				list.add(plus);
+			}
+			
+			
+			for(int i = 0; i  < list.size()-1 ; i++) {
+				if(list.get(i) >= 10){	
+					//use the [java.util.ArrayList.set(index , element) method]
+					list.set( i+1 ,list.get(i+1) +1);
+					list.set(i , list.get(i) % 10);
+			
+				}
+			}
+		
+			//print the ans
+			System.out.print("the fist number plus the second number is = ");
+			//print the number 
+			for(int j = list.size()-1 ; j >=0 ; j--){
+				System.out.print(list.get(j));
+			}
+		}
+	
+	}
+}
